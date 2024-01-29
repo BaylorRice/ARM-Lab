bbc8501406192d52cc8576cd3c8c994e08f0806b
Reese Ford <reese_ford1@baylor.edu>
2024-01-29 12:34:11 -0600

    Finished mux.v
    Added the logic? It may work

--- ARM-Lab/code/0_common/mux.v
+++ ARM-Lab/code/0_common/mux.v
@@ -6,8 +6,13 @@ module mux#(
     input [SIZE-1:0] b_in,
     input control,
     output [SIZE-1:0] mux_out
-    );
-	
-	// TODO: Add body of mux here
-    
+);
+
+    // TODO: Add body of mux here
+    if (control) begin
+        assign mux_out = a_in;
+    end else begin
+        assign mux_out = b_in;
+    end
+
 endmodule
