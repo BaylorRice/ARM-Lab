a3109a8bc13f2a67bcfb1a1e889de6fef5c57b2a
Reese Ford <reese_ford1@baylor.edu>
2024-01-31 13:35:14 -0600

    Created iFetch.v

---
+++ ARM-Lab/code/1_fetch/iFetch.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Baylor University
+// Engineer: Reese Ford
+// 
+// Create Date: 01/31/2024 01:33:58 PM
+// Design Name: Fetch Stage
+// Module Name: iFetch
+// Project Name: Lab03 - Fetch Stage
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module iFetch(
+
+    );
+endmodule
