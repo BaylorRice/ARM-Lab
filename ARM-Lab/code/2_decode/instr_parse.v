ee9d92b9cf015eabcc39f3d1037e5764fa5ce495
Reese Ford <reese_ford1@baylor.edu>
2024-02-12 12:48:16 -0600

    Created instr_parse.v

---
+++ ARM-Lab/code/2_decode/instr_parse.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+`include "definitions.vh"
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Baylor University
+// Engineer: Reese Ford
+// 
+// Create Date: 02/12/2024 12:46:26 PM
+// Design Name: Isntruction Parse
+// Module Name: instr_parse
+// Project Name: Lab 4 - Beginning to Decode
+// Target Devices: Vivado Simulation
+// Tool Versions: Vivado 2021.2
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module instr_parse(
+
+    );
+endmodule
