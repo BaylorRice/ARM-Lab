98b4c82e243360284a19c63652cdad48af0ea257
Reese Ford <reese_ford1@baylor.edu>
2024-02-12 13:29:46 -0600

    Update instr_parse_test.sv
    Added verify for TC 1

--- ARM-Lab/code/2_decode/instr_parse_test.sv
+++ ARM-Lab/code/2_decode/instr_parse_test.sv
@@ -13,11 +13,19 @@ wire [4:0] rd_num;
 
 // create er signals that have the same names of the signals above except that they
 // have er_ appended to the beginning of the name....for example, er_rm_num
-wire [10:0] er_opcode;
-wire [8:0] er_address;
-wire [4:0] er_rm_num;
-wire [4:0] er_rn_num;
-wire [4:0] er_rd_num;
+reg [10:0] er_opcode;
+reg [8:0] er_address;
+reg [4:0] er_rm_num;
+reg [4:0] er_rn_num;
+reg [4:0] er_rd_num;
+
+string opcode_string = "|opcode|";
+string address_string = "|address|";
+string rm_num_string = "|rm_num|";
+string rn_num_string = "|rn_num|";
+string rd_num_string = "|rd_num|";
+int ts = 0;
+int tc = 0;
 
 // use this instruction_parse instance for this test
 instr_parse parser(
@@ -45,6 +53,7 @@ begin_test();
 ///// TEST CASE 1 /////
 $display("Test Case %0d", tc++);
 $display("LDUR X9, [X22, #240]");
+ts = 0;
 
 // set instruction to the binary equivalent of the instruction listed above
 // 1986 240 0 22 9
@@ -52,18 +61,27 @@ $display("LDUR X9, [X22, #240]");
 instruction = `INSTR_LEN'hF84F02C9;
 
 // wait for 2 ns
+#2
 
 // DO NOT verify rm_num is correct because it is not relevant for a D Type instruction
 // verify rn_num is correct
+er_rn_num = 5'd22;
+verify(ts++, rn_num_string, er_rn_num, $bits(er_rn_num), rn_num, $bits(rn_num), `S_DEC);
 
 // verify rd_num is correct
+er_rd_num = 5'd9;
+verify(ts++, rd_num_string, er_rd_num, $bits(er_rd_num), rd_num, $bits(rd_num), `S_DEC);
 
 // verify address is correct
+er_address = 9'd240;
+verify(ts++, address_string, er_address, $bits(er_address), address, $bits(address), `S_DEC);
 
 // verify opcode is correct
+er_opcode = 11'd1986;
+verify(ts++, opcode_string, er_opcode, $bits(er_opcode), opcode, $bits(opcode), `S_DEC);
 
 // wait for 8 ns
-
+#8
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // ADD X10, X21, X9
@@ -71,6 +89,7 @@ instruction = `INSTR_LEN'hF84F02C9;
 ///// TEST CASE 2 /////
 $display("\nTest Case %0d", tc++);
 $display("ADD X10, X21, X9");
+ts = 0;
 
 // set instruction to the binary equivalent of the instruction listed above
 // 1112 9 0 21 10
