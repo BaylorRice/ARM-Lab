b3e18cdcf70823417d4cb5e90f06b71c11d9e903
Reese Ford <reese_ford1@baylor.edu>
2024-02-12 13:52:55 -0600

    Created regfile.v

---
+++ ARM-Lab/code/2_decode/regfile.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+`include "definitions.vh"
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Baylor University
+// Engineer: Reese Ford
+// 
+// Create Date: 02/12/2024 01:50:24 PM
+// Design Name: Register File
+// Module Name: regfile
+// Project Name: Lab 4 - Beginnning to Decode
+// Target Devices: Vivado Simulation
+// Tool Versions: Vivado 2021.2
+// Description: *shrug*
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module regfile(
+
+    );
+endmodule
