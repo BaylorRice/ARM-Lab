610da3adf7793fa44f46dd56c0f7d299a2792aca
Reese Ford <reese_ford1@baylor.edu>
2024-02-12 14:14:35 -0600

    Updated regfile.v
    Finished?

--- ARM-Lab/code/2_decode/regfile.v
+++ ARM-Lab/code/2_decode/regfile.v
@@ -22,6 +22,31 @@
 
 
 module regfile(
+    input wire read_clk,
+    input wire write_clk,
+    input wire [4:0] read_register1,
+    input wire [4:0] read_register2,
+    input wire [4:0] write_register,
+    input wire [`WORD-1:0] write_data,
+    input wire reg_write,
+    output reg [`WORD-1:0] read_data2,
+    output reg [`WORD-1:0] read_data2,
+);
 
-    );
+    reg[`WORD-1:0] rmem [31:0];
+
+    initial
+    $readmemh(`RMEMFILE, rmem);
+
+    always @(posedge read_clk) begin
+        read_data1 = rmem[read_register1];
+        read_data2 = rmem[read_register2];
+    end
+
+    always @(posedge write_clk) begin
+        if (reg_write)
+            rmem[write_register] = write_data;
+    end
+
+
 endmodule
