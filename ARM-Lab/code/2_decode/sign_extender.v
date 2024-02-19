6d6c28023e3fe348e5a801963868f6aea10fc00f
Reese Ford <reese_ford1@baylor.edu>
2024-02-19 13:49:04 -0600

    Created sign_extender.v

---
+++ ARM-Lab/code/2_decode/sign_extender.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Baylor University
+// Engineer: Reese Ford
+// 
+// Create Date: 02/19/2024 01:47:03 PM
+// Design Name: Sign Extender
+// Module Name: sign_extender
+// Project Name: Lab 5 - Control unit and Sign Extenders
+// Target Devices: Vivado Simulation
+// Tool Versions: Vivado 2021.2
+// Description: figure it out
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sign_extender(
+    input [`INSTR_LEN-1:0] instruction,
+    output reg [`WORD-1:0] sign_extended_output
+    );
+    
+    
+endmodule
