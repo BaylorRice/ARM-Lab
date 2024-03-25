80b75e8723e2504488ec6df6a1dc893db09e0e66
Reese Ford <reese_ford1@baylor.edu>
2024-03-25 12:29:58 -0500

    Created ALU.v

---
+++ ARM-Lab/code/3_execute/alu.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Baylor University
+// Engineer: Reese Ford
+// 
+// Create Date: 03/25/2024 12:28:52 PM
+// Design Name: Aritmatic Logic Unit ("ALU")
+// Module Name: alu
+// Project Name: Lab 8 - ALU and ALU Control
+// Target Devices: Vivado Simulator
+// Tool Versions: Vivado 2021.2
+// Description: figure it out
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu(
+
+    );
+endmodule
