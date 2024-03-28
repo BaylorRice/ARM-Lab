8b2e867062d7ad2199e9137b2662672455035ed2
Reese Ford <reese_ford1@baylor.edu>
2024-03-28 13:49:15 -0500

    Sim Successful

--- ARM-Project/ARM-Project.sim/sim_1/behav/xsim/xsim.dir/iExecute_test_behav/obj/xsim_1.c
+++ ARM-Project/ARM-Project.sim/sim_1/behav/xsim/xsim.dir/iExecute_test_behav/obj/xsim_1.c
@@ -100,7 +100,7 @@ void sensitize(char *dp)
 
 void simulate(char *dp)
 {
-iki_register_root_pointers(4, 11384, -5,0,11568, -5,0,11752, -5,0,8008, -5,0) ; 
+iki_register_root_pointers(4, 11752, -5,0,11568, -5,0,11384, -5,0,8008, -5,0) ; 
 		iki_schedule_processes_at_time_zero(dp, "xsim.dir/iExecute_test_behav/xsim.reloc");
 	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
 	iki_execute_processes();
