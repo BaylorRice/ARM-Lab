4a16d2c9b35b6fb02db4a510de4967cb22b7d92f
Reese Ford <reese_ford1@baylor.edu>
2024-04-03 13:59:09 -0500

    Created iMemory.v

---
+++ ARM-Lab/code/4_memory/iMemory.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Baylor University
+// Engineer: Reese Ford
+// 
+// Create Date: 04/03/2024 01:56:58 PM
+// Design Name: Memory Module
+// Module Name: iMemory
+// Project Name: Lab 10 - Memory Stage
+// Target Devices: Vivado Simulator
+// Tool Versions: Vivado 2021.2
+// Description: it remembers. what more do you want
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module iMemory(
+
+    );
+endmodule
