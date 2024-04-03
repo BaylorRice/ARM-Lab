f4cee6ffad397cda6745b3a1fee4d221d042e923
Reese Ford <75872275+RicePerson@users.noreply.github.com>
2024-04-03 15:01:13 -0500

    Ran sim
    10/10 pass rate

--- ARM-Project/ARM-Project.sim/sim_1/behav/xsim/xsim.dir/iMemory_test_behav/obj/xsim_1.c
+++ ARM-Project/ARM-Project.sim/sim_1/behav/xsim/xsim.dir/iMemory_test_behav/obj/xsim_1.c
@@ -99,7 +99,7 @@ void sensitize(char *dp)
 
 void simulate(char *dp)
 {
-iki_register_root_pointers(3, 10544, -5,0,7376, -5,0,10728, -5,0) ; 
+iki_register_root_pointers(3, 7376, -5,0,10544, -5,0,10728, -5,0) ; 
 		iki_schedule_processes_at_time_zero(dp, "xsim.dir/iMemory_test_behav/xsim.reloc");
 	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
 	iki_execute_processes();
