1d2e902865cf41b8f90fe644e107dda9c89a8bb9
Reese Ford <reese_ford1@baylor.edu>
2024-04-03 14:09:27 -0500

    Updated iMemory.v

--- ARM-Lab/code/4_memory/iMemory.v
+++ ARM-Lab/code/4_memory/iMemory.v
@@ -35,6 +35,20 @@ module iMemory(
     output reg pc_src
     );
     
-    
+    reg[`WORD-1:0] rmem [99:0];
+
+    initial
+    $readmemb(`DMEMFILE, dmem);
+
+    always @(posedge read_clk) begin
+        read_data1 = dmem[read_register1];
+        read_data2 = dmem[read_register2];
+    end
+
+    always @(posedge write_clk) begin
+        if (reg_write == 1) begin
+            dmem[write_register] = write_data;
+        end
+    end
     
 endmodule
